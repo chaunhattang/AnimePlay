896b4794a37375fd27ccb52585812fe5c8e3a749
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2026-04-22 09:42:20 +0700

    Init Backend

---
+++ backend/AnimePlay/src/main/java/com/backend/animeplay/AnimePlayApplication.java
@@ -0,0 +1,13 @@
+package com.backend.animeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimePlayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimePlayApplication.class, args);
+        System.out.println("========Successfully Started=========");
+    }
+
+}
---
+++ backend/AnimePlay/src/test/java/com/backend/animeplay/AnimePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.animeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimePlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
