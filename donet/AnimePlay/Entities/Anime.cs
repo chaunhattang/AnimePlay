082ea127f2f97679db4f6359c499d0af3c44321b
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2026-04-29 15:43:39 +0700

    init backened

---
+++ donet/AnimePlay/Entities/Anime.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimePlay.Entities
+{
+    [Table("Animes")]
+    public class Anime
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        [Column(TypeName = "text")] 
+        public string? Description { get; set; }
+
+        public string? Year { get; set; }
+
+        public string? Genre { get; set; }
+
+        public string? PosterUrl { get; set; }
+
+        public string? TrailerUrl { get; set; }
+    }
+}
---
+++ donet/AnimePlay/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+// using AnimePlay.Enums; // Mở comment này khi bạn tạo thư mục Enums
+
+namespace AnimePlay.Entities
+{
+    [Table("Users")]
+    [Index(nameof(Username), IsUnique = true)] // Tương đương unique = true
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required] // Tương đương nullable = false
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        public string? FullName { get; set; }
+
+        [Column(TypeName = "text")]
+        public string? AvatarUrl { get; set; }
+
+        // public RoleEnum Role { get; set; } // Cần tạo thêm file RoleEnum.cs trong thư mục Enums
+        public int Role { get; set; } // Tạm thời để int, bạn sửa lại sau khi có Enum nhé
+    }
+}
